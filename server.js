0050a54da4cf10eb666ebe69508136a7fb6d85c4
naka253 <mr.kota0525@gmail.com>
2026-05-18 21:55:31 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,35 +2,88 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 
+const fs = require("fs");
+const path = require("path");
+
 const app = express();
 
 const server = http.createServer(app);
 
 const io = new Server(server);
 
-/* index.htmlを公開 */
+/* HTML公開 */
 app.use(express.static(__dirname));
 
-/* 接続 */
+/* 履歴ファイル */
+const HISTORY_FILE = path.join(__dirname, "messages.json");
+
+/* 履歴読み込み */
+let messages = [];
+
+if(fs.existsSync(HISTORY_FILE)){
+
+  try{
+    const data = fs.readFileSync(HISTORY_FILE, "utf8");
+    messages = JSON.parse(data);
+  }catch(err){
+    console.log("履歴読み込み失敗");
+    messages = [];
+  }
+}
+
+/* 履歴保存 */
+function saveMessages(){
+
+  fs.writeFileSync(
+    HISTORY_FILE,
+    JSON.stringify(messages, null, 2)
+  );
+}
+
+/* Socket.IO */
 io.on("connection", (socket) => {
 
-  console.log("ユーザー接続");
+  console.log("接続");
+
+  /* 接続時に履歴送信 */
+  socket.emit("chat history", messages);
 
   /* 入室 */
   socket.on("join", (username) => {
 
     socket.username = username;
 
-    io.emit(
-      "system message",
-      `${username} さんが入室しました`
-    );
+    const systemMessage = {
+      type: "system",
+      text: `${username} さんが入室しました`
+    };
+
+    messages.push(systemMessage);
+
+    saveMessages();
+
+    io.emit("system message", systemMessage.text);
   });
 
   /* メッセージ */
   socket.on("chat message", (data) => {
 
-    io.emit("chat message", data);
+    const messageData = {
+      type: "chat",
+      username: data.username,
+      text: data.text
+    };
+
+    messages.push(messageData);
+
+    /* 最大500件 */
+    if(messages.length > 500){
+      messages.shift();
+    }
+
+    saveMessages();
+
+    io.emit("chat message", messageData);
   });
 
   /* 切断 */
@@ -38,13 +91,19 @@ io.on("connection", (socket) => {
 
     if(socket.username){
 
-      io.emit(
-        "system message",
-        `${socket.username} さんが退出しました`
-      );
+      const systemMessage = {
+        type: "system",
+        text: `${socket.username} さんが退出しました`
+      };
+
+      messages.push(systemMessage);
+
+      saveMessages();
+
+      io.emit("system message", systemMessage.text);
     }
 
-    console.log("ユーザー切断");
+    console.log("切断");
   });
 });
 
